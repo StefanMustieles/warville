aac2db225db2717b150a698071b08e22a8f5c381
d-unit <danieldevelop@outlook.com>
2016-11-16 20:08:23 +0000

    Add Belgian divisions

---
+++ belgian/divisions/index.php
@@ -0,0 +1,154 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/page.inc';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/dbconfig.php'; 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/db.php';
+
+$homepage = new Page();
+
+$homepage->contentpagescripts = '<script src="/assets/js/listedItems.js" type="text/javascript"></script>';
+
+$db = new Zebra_Database();
+
+$db->connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+$uri = $_SERVER["REQUEST_URI"];
+$uriParts = explode("/", $uri);
+$isSubCategory = true;
+
+if($uriParts[3] == '' || strpos($uriParts[3], 'index') !== FALSE) {
+    $isSubCategory = false;
+}
+
+if(!$isSubCategory) {
+    $db->query('SELECT t1.description, t2.name AS country, t1.name, "" AS sub_category, t1.meta_description, t1.page_title '
+            . 'FROM categories AS t1 INNER JOIN countries AS t2 ON t1.country_id = t2.country_id '
+            . 'WHERE t2.country_id = ? AND t1.category_id = ?', array(2, 151));
+}
+else {
+    $db->query('SELECT t1.description, t3.name AS country, t2.name, t1.name AS sub_category, t1.meta_description, t1.page_title '
+             . 'FROM sub_categories AS t1 INNER JOIN categories AS t2 ON t1.category_id = t2.category_id '
+             . 'INNER JOIN countries AS t3 ON t2.country_id = t3.country_id '
+             . 'WHERE t3.country_id = ? AND t1.seo_url = LOWER(?)', array(2, $uriParts[3]));
+}
+												
+while ($row = $db->fetch_assoc()) {
+
+$descriptionText = $row["description"];							
+$country = $row["country"];
+$category = $row["name"];
+$sub_category = $row["sub_category"];
+$metaDescription = $row["meta_description"];
+$page_title = $row["page_title"];
+}
+
+$homepage->metadescription = $metaDescription;
+
+$postContent = sprintf('<section id="content">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <ul class="breadcrumb">
+                                            <li><a href="../../">Home</a></li>
+                                            <li><a href="../">%s</a></li>', $country);
+if(!$isSubCategory) {
+    $postContent .= sprintf('<li class="active">%s</li>', $category);
+    $title = $country . ' ' . $category;
+}
+else {
+    $postContent .= sprintf('<li><a href="' . substr($_SERVER['REQUEST_URI'], 0, strripos($_SERVER['REQUEST_URI'], "/")) . '/">%s</a></li>
+                             <li class="active">%s</li>', $category, $sub_category);
+    $title = $country . ' ' . $sub_category;
+}
+
+$postContent .= sprintf('</ul>
+                        <h1 id="contentHeader">%s</h1>
+                        <p id="description">%s</p>
+                    </div>
+                </div><!--/.row-->
+            </div><!--/.container-->
+    </section><!--/#content-->
+
+    <section id="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <p class="lead">Filters</p>
+                        <div class="list-group">
+                            <a id="All" class="list-group-item%s">All</a>', $title, $descriptionText, $isSubCategory == false ? ' active' : '');
+
+$db->select(
+    'sub_category_id, name, seo_url',
+    'sub_categories',
+    'category_id = ? AND active = 1', array(151), 'sort_order'
+);
+												
+while ($row = $db->fetch_assoc()) {
+
+    if($isSubCategory && $uriParts[3] == $row["seo_url"])
+        $postContent .=	 '<a id="' . $row["sub_category_id"] . '" href="' . $row["seo_url"] . '" class="list-group-item active">' . $row["name"] . '</a>';
+    else
+        $postContent .=	 '<a id="' . $row["sub_category_id"] . '" href="' . $row["seo_url"] . '" class="list-group-item">' . $row["name"] . '</a>';                                                       
+}
+														
+$postContent .= '</div>
+		</div>
+
+		<div class="col-md-9 itemHolder">
+                    <div class="row">';
+						
+if(!$isSubCategory) {
+    $db->query(
+        'SELECT t1.sub_category_id, t1.item_id, t1.title, t1.friendly_url, t1.thumbnail_image, t1.short_text FROM `items` AS t1 '
+          . 'INNER JOIN sub_categories AS t2 ON t1.sub_category_id = t2.sub_category_id '
+            . 'WHERE t2.category_id = ? GROUP BY t1.sub_category_id, t1.item_id ORDER BY t2.sort_order, t1.display_order', array(151)
+    );
+}
+else {
+    $db->query(
+        'SELECT t1.sub_category_id, t1.item_id, t1.title, t1.friendly_url, t1.thumbnail_image, t1.short_text FROM `items` AS t1 '
+          . 'INNER JOIN sub_categories AS t2 ON t1.sub_category_id = t2.sub_category_id '
+            . 'WHERE t2.category_id = ? AND t2.seo_url = LOWER(?) ORDER BY t2.sort_order, t1.display_order', array(151, $uriParts[3])
+    );
+}
+
+$i = 1;
+
+while ($row = $db->fetch_assoc()) {
+
+    $postContent .= '<div class="col-sm-4 col-lg-4 col-md-4 item" data-id="id-' . $row["item_id"] . '" data-type="' . $row["sub_category_id"] . '">
+                        <a href="' . $row["item_id"] . '/' . $row["friendly_url"] . '">    
+                            <div class="thumbnail">';
+
+    if(empty($row["thumbnail_image"]))
+        $postContent .= '<img src="/assets/images/awaitingImage.jpg" alt="' . $row["title"] . '" class="thumbnail-pics">';
+    else
+        $postContent .= '<img src="img/' . $row["thumbnail_image"] . '" alt="' . $row["title"] . '" class="thumbnail-pics">';
+    
+    $postContent .='<div class="caption">
+                        <h4>' . $row["title"] . '</h4>
+                        <p>' . $row["short_text"] . '</p>
+                    </div>
+                </div>
+            </a>
+        </div>';
+    
+    if($i % 3 == 0){
+       $postContent .= '<div class="clearfix visible-xs-block"></div>';
+    }
+    $i++;
+}
+						
+$postContent .= '</div><!--/.row-->
+        </div><!--/.col-md-9-->
+    </div><!--/.container-->
+</section><!--/#content-->';
+
+if(empty($page_title)) $homepage->title = $title . ' - ' . $homepage->title;
+else $homepage->title = $page_title;
+
+$homepage->canonical = '<link rel="canonical" href="http://' . $_SERVER["HTTP_HOST"] . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . '" />';
+
+$homepage->content = $postContent;
+	
+$homepage->Display();
---
+++ belgian/divisions/item.php
@@ -0,0 +1,100 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/page.inc';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/dbconfig.php'; 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/templater.php';
+
+$homepage = new Page();
+
+$db = new Zebra_Database();
+
+$db->connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+$db->query('SELECT t1.item_id, t1.name, t1.large_image, t4.name AS category_name, t5.name AS country_name, t2.file_name AS template_name, t1.content, t1.image_source, t1.views '
+        . 'FROM items AS t1 INNER JOIN templates AS t2 ON t1.template_id = t2.template_id INNER JOIN sub_categories AS t3 ON t1.sub_category_id = t3.sub_category_id INNER JOIN categories AS t4 ON t3.category_id = t4.category_id INNER JOIN countries AS t5 ON t4.country_id = t5.country_id '
+        . 'WHERE t1.item_id = ?', array($_GET["itemId"])
+);
+
+while ($row = $db->fetch_assoc()) {
+
+    $itemName = $row['name'];
+    $large_image = $row['large_image'];
+    $category_name = $row['category_name'];
+    $country_name = $row['country_name'];
+    $templateName = $row['template_name'];
+    $content = $row['content'];
+    $imageSource = $row['image_source'];
+    $views = $row['views'];
+}
+
+$tpl =  new templater($_SERVER['DOCUMENT_ROOT'] . '/templates/' . $templateName);
+$tpl->set("content", $content);
+$imageCaption;
+if(!isset($imageSource) || trim($imageSource)==='') {
+    $imageCaption = "";
+}
+else {
+    $imageCaption = "Image: " . $imageSource;
+}
+
+if($location = substr(dirname($_SERVER['PHP_SELF']), 1))
+    $dirlist = explode('/', $location);
+else
+    $dirlist = array();
+
+$count = array_push($dirlist, basename($_SERVER['PHP_SELF']));
+
+$address = 'http://'.$_SERVER['HTTP_HOST'];
+
+$content = '<section id="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <ul class="breadcrumb">
+                                <li><a href="' . $address . '">Home</a></li>
+                                <li><a href="' .($address .= '/'.$dirlist[0]) . '/">%s</a></li>
+                                <li><a href="' .($address .= '/'.$dirlist[1]) . '/">%s</a></li>
+                                <li class="active">%s</li>
+                            </ul>
+                            <h1>%s</h1>
+                        </div><!--/.col-md-12-->
+                    </div><!--/.row-->
+                </div><!--/.container-->
+            </section><!--/#content-->
+            <section id="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="thumbnail clearfix">';
+                            
+    if(empty($large_image))
+        $content .= '<img src="/assets/images/awaitingImage.jpg" alt="%s" class="img-responsive pull-left largeImage">';
+    else
+        $content .= '<img src="../img/' . $large_image . '" alt="%s" class="img-responsive pull-left largeImage">';
+                                
+        $content .= '<div class="caption largeImageCaption" class="pull-right">'
+                        . $imageCaption .
+                    '</div>
+                </div>
+                <div class="caption-full">'
+                     . $tpl->output() . 
+                '</div>
+            </div><!--/.col-md-12-->
+        </div><!--/.row-->
+    </div><!--/.container-->
+</section><!--/#content-->';
+
+$pageContent = sprintf($content, $country_name, $category_name, $itemName, $itemName, $itemName);
+
+$homepage->content = $pageContent;
+
+$homepage->title = $itemName . ' - ' . $homepage->title;
+
+$homepage->canonical = '<link rel="canonical" href="http://' . $_SERVER["HTTP_HOST"] . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . '" />';
+
+$db->update('items', 
+            array('views' => $views + 1),
+            'item_id = ?', array($_GET["itemId"]));
+
+$homepage->Display();
